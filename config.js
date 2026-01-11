f71ac4016e6117b2f9bc852fab5afdd2a184a161
MVXRVSC <danieliithothekingpromo.09@gmail.com>
2026-01-11 08:43:25 -0500

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,10 @@
+export const config = {
+  botName: "ORBIT-MD",
+  ownerName: "ORBIT Owner",
+  ownerNumber: ["+535XXXXXXX"],
+  prefix: [".", "!", "/"],
+  version: "1.0.0",
+  sessionName: "orbit-session",
+  mode: "public"
+}
+
