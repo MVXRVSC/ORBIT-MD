a16e117112ca2171485bf5900d979a67ea742f14
SLAYER-MD <danieliithothekingpromo.09@gmail.com>
2026-01-11 12:43:24 -0500

    Create gitignore.py

---
+++ gitignore.py
@@ -0,0 +1,87 @@
+# ----------------------------
+# ORBIT-MD Gitignore adaptado
+# ----------------------------
+
+# Logs
+logs/
+*.log
+npm-debug.log*
+yarn-debug.log*
+lerna-debug.log*
+.pnpm-debug.log*
+
+# Diagnostic reports
+report.[0-9]*.[0-9]*.[0-9]*.[0-9]*.json
+
+# Runtime data
+pids/
+*.pid
+*.seed
+*.pid.lock
+
+# Coverage and instrumented libs
+lib-cov/
+coverage/
+*.lcov
+.nyc_output/
+
+# Grunt / Bower / Build caches
+.grunt/
+bower_components/
+build/Release/
+jspm_packages/
+web_modules/
+
+# Snowpack / Parcel / TypeScript / ESLint / Stylelint
+.cache/
+.parcel-cache/
+*.tsbuildinfo
+.npm/
+.eslintcache
+.stylelintcache
+.rpt2_cache/
+.rts2_cache_cjs/
+.rts2_cache_es/
+.rts2_cache_umd/
+
+# Optional REPL / Yarn
+.node_repl_history
+*.tgz
+.yarn-integrity
+.pnp.*
+
+# Environment files
+.env
+.env.development.local
+.env.test.local
+.env.production.local
+.env.local
+
+# Node / Nuxt / Next / Gatsby / Vuepress / Docusaurus builds
+.next/
+out/
+.nuxt/
+dist/
+.cache/
+.vuepress/dist/
+.temp/
+.docusaurus/
+
+# Serverless / FuseBox / DynamoDB
+.serverless/
+.fusebox/
+.dynamodb/
+
+# TernJS / VSCode test
+.tern-port
+.vscode-test/
+
+# ORBIT-MD specific
+sessions/
+sesionRespaldo/
+tmp/
+media/
+database.json
+data.store.json
+package-lock.json
+
